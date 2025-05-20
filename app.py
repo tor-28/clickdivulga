11cd7aa5a25bde4d33e42e06c91e419a2245164e
tor-28 <lourencomenezes63@gmail.com>
2025-05-19 22:33:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 import os
